ea9ce139099cd9ea4ab4d8752fe3590e0d9884a6
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-01-11 16:12:57 +0100

    Remove horizontal scrollbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 $(function() {
     var carouselList = $("#carousel ul");
-    
     // uruchom funkcję changeSlide() co 3 sekundy
     function picsInterval() {
-        setInterval(changeSlide, 6000);
+        setInterval(changeSlide, 3000);
     }
-    
     // animacja CSS slajdu, trwa 0,5 sekundy, na koniec moveFirstSlide
     function changeSlide() {
         carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
     }
-    
     // funkcja callback do wykonania na koniec animacji.
     // znajdź 1 list item (img) , znajdź ostatni list item (img)
     function moveFirstSlide() {     
@@ -19,6 +16,5 @@ $(function() {
         lastItem.after(firstItem);        // dodaj pierwszy img po ostatnim img
         carouselList.css({ marginLeft: 0 });                    // reset marginesu 
     }
-    
     picsInterval();
 });

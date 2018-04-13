bc674991db9a73742e82882f550d2c4bc7e43cb0
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-04-13 19:45:22 +0200

    Add proper CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 $(function() {
     var carouselList = $("#carousel ul");
-    // uruchom funkcję changeSlide() co 3 sekundy
+    
     function picsInterval() {
         setInterval(changeSlide, 3000);
     }
-    // animacja CSS slajdu, trwa 0,5 sekundy, na koniec moveFirstSlide
+    
     function changeSlide() {
         carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
     }
-    // funkcja callback do wykonania na koniec animacji.
-    // znajdź 1 list item (img) , znajdź ostatni list item (img)
+    
     function moveFirstSlide() {     
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
-        lastItem.after(firstItem);        // dodaj pierwszy img po ostatnim img
-        carouselList.css({ marginLeft: 0 });                    // reset marginesu 
+        lastItem.after(firstItem);
+        carouselList.css({ marginLeft: 0 });
     }
     picsInterval();
 });
